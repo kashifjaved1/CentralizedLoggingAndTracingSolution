3690b3f7e6a0c995f8e24eebc74193b3c89d9020
MKashifJaved <MKashifJaved@DESKTOP-HN1QNFN>
2024-07-23 04:13:09 +0500

    refactoring: removed unnecessary usings

--- Core/Core/Data/Entities/Response.cs
+++ Core/Core/Data/Entities/Response.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.Data.Entities
+﻿namespace Core.Data.Entities
 {
     public class Response : DefaultEntity
     {
